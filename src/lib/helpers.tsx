e5e58fe29fdd1a3b75b141b7d7c1444b13c09bf8
Delight Sheriff <delightsheriff@gmail.com>
2025-07-18 22:38:20 +0100

    added helper funcions to getToday's activietes

--- src/lib/helpers.tsx
+++ src/lib/helpers.tsx
@@ -16,6 +16,20 @@ export const formatDate = (dateString: string) => {
   });
 };
 
+// Supabase needs an ISO date string. However, that string will be different on every render because the MS or SEC have changed, which isn't good. So we use this trick to remove any time
+type GetTodayOptions = { end?: boolean };
+
+export const getToday = function (options: GetTodayOptions = {}) {
+  const today = new Date();
+
+  // This is necessary to compare with created_at from Supabase, because it it not at 0.0.0.0, so we need to set the date to be END of the day when we compare it with earlier dates
+  if (options?.end)
+    // Set to the last second of the day
+    today.setUTCHours(23, 59, 59, 999);
+  else today.setUTCHours(0, 0, 0, 0);
+  return today.toISOString();
+};
+
 export const getStatusBadge = (status: Bookings["status"]) => {
   switch (status) {
     case "unconfirmed":
--- src/lib/services/Bookings.ts
+++ src/lib/services/Bookings.ts
@@ -1,5 +1,6 @@
 import type { Bookings } from "@/interface/bookings";
 import supabase from "./supabase";
+import { getToday } from "../helpers";
 
 export interface BookingsQueryParams {
   page?: number;
@@ -103,6 +104,27 @@ export async function updateBooking(id: string, obj: Partial<Bookings>) {
   if (error) {
     console.error(error);
     throw new Error("Booking could not be updated");
+  }
+  return data;
+}
+
+// Activity means that there is a check in or a check out today
+export async function getStaysTodayActivity() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, guests(fullName, nationality, countryFlag)")
+    .or(
+      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
+    )
+    .order("created_at");
+
+  // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
+  // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
+  // (stay.status === 'checked-in' && isToday(new Date(stay.endDate)))
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
   }
   return data;
 }
